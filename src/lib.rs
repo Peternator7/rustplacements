037526eda8e3b083dfcee7197aab0f4d202b5cf9
Peternator7 <glotfelty.2@osu.edu>
2017-08-26 23:36:30 -0700

    Shaped up the Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,101 @@
 #![crate_type = "dylib"]
 #![feature(plugin_registrar, rustc_private)]
 
+//! # Rustplacements
+//!
+//! This is a compiler plugin for the [Rust language](https://www.rust-lang.org/en-US/) that replaces all of your string literals
+//! in the source code with random text. Well, it's not really random. You can choose to replace text with items from any of the
+//! lists on [this page](https://github.com/Peternator7/rustplacements/blob/master/CATEGORIES.md) by simply adding a few
+//! attributes to your existing Rust code.
+//!
+//! ## A Brief Example
+//!
+//! Let's start with a simple example like the one below. It prints out the words in the sentence below one word at a time.
+//!
+//! ```rust,ignore
+//! const SENTENCE: [&'static str; 9] = ["The", "Quick", "Brown", "Fox", "Jumped", "Over", "the",
+//!                                      "Lazy", "Dog"];
+//!
+//! fn main() {
+//!     for word in &SENTENCE {
+//!         println!("{}", word);
+//!     }
+//! }
+//! ```
+//!
+//! The output should look like:
+//!
+//! ```txt
+//! The
+//! Quick
+//! Brown
+//! Fox
+//! Jumped
+//! Over
+//! the
+//! Lazy
+//! Dog
+//! ```
+//!
+//! Rustplacements let's us replace all the strings at compile with other values. Let's say we want to replace all the text with
+//! emojis. Rustplacements can do that.
+//!
+//! ```rust,ignore
+//! #![feature(plugin)]
+//! #![plugin(rustplacements)]
+//!
+//! // Placing it in the module root will replace everything in the module
+//! #![Rustplacements = "emojis"]
+//!
+//! const SENTENCE: [&'static str; 9] = ["The", "Quick", "Brown", "Fox", "Jumped", "Over", "the",
+//!                                      "Lazy", "Dog"];
+//!
+//! fn main() {
+//!     for word in &SENTENCE {
+//!         println!("{}", word);
+//!     }
+//! }
+//! ```
+//!
+//! The new output will look something like this. The output is randomized so it will be re-generated everytime you compile
+//! your crate.
+//!
+//! ```text
+//! 😢 😫 🤓
+//! 😞 😠 😟 😖 😧
+//! 😬 😬 😈 😡 😟
+//! 😓 😒 😬
+//! 😝 😘 🤧 😬 😧 😡
+//! 😗 😈 😉 😫
+//! 😄 😱 😰
+//! 😃 🤡 😅 😯
+//! 🤒 😈 😈
+//! ```
+//!
+//! ## Using Rustplacements
+//!
+//! Compiler plugins like Rustplacements are only available on nightly rust because they require a feature flag to use. To get started,
+//! Rustplacements is available on [crates.io](https://crates.io/crates/rustplacements). To download the latest version, add the
+//! following line to the `Cargo.toml`.
+//!
+//! ```toml
+//! [dependencies]
+//! rustplacements = "*"
+//! ```
+//!
+//! To enable the compiler plugin, add the following lines on the top of your `main.rs` or `lib.rs`.
+//!
+//! ```rust,ignore
+//! #![feature(plugin)]
+//! #![plugin(rustplacements)]
+//! ```
+//!
+//! You can now use the plugin anywhere in the crate by applying the `#[Rustplacements = "one-direction"]` to any language element.
+//! You can place the element in the root with `#![Rustplacements = "got-quotes"]` and it will transform all the string literals
+//! in your module. It can also be applied to specific strings / impls / functions for more fine grained control.
+//!
+//! That's pretty much all there is to it. Check out the [categories page](https://github.com/Peternator7/rustplacements/blob/master/CATEGORIES.md) for more categories that you can use.
+
 extern crate rustc;
 extern crate syntax;
 extern crate syntax_ext;
